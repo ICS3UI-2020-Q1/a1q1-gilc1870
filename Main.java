946b8798110b417d6e72100b49b4ec35f765aef5
gilc1870 <gilc1870@wrdsb.ca>
2020-10-13 15:01:56 +0000

    Converts C to F

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
  *
- * @author 
+ * @author Cesar Gil
  */
 public class Main {
 
@@ -9,7 +11,15 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // To convert a temperature from Celsius to Fahrenheit
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please give tempture you would like converted from Celsius to Fahrenheit");
+    
+    int degrees = input.nextInt();
+    int Fahrenheit = ((degrees*9)/5 +32);
+    
+    System.out.println(degrees + "C is equal to " + Fahrenheit + "F");
     
   }
 }
